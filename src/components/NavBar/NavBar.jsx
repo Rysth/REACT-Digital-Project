ea048e86a6090c929cce3dbe7b23bbf7a6bfedf8
Rysth <johnpalacios.t@gmail.com>
2023-10-18 12:24:55 -0500

    Add mobile menu.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,44 +1,42 @@
 import { NavLink } from 'react-router-dom';
 import Logo from '../../assets/images/logo.svg';
+import NavigationLinks from '../../data/navigation.json';
 import './NavBar.css';
 
 function NavBar() {
   return (
     <div>
-      <div className="container max-w-screen-xl mx-auto p-4 sm:py-6 flex items-center justify-between">
+      <div className="container max-w-screen-xl mx-auto p-4 sm:py-6 flex flex-wrap items-center justify-between">
         <a href="/" rel="noopener noreferrer">
           <img src={Logo} alt="" className="w-16 sm:w-24" />
         </a>
-        <nav>
-          <ul className="p-0 text-[var(--CL-primary-gray)] flex items-center gap-5">
-            <li className="uppercase text-xs ">
-              <NavLink to="/" className="tracking-widest px-4 py-1">
-                Main
-              </NavLink>
-            </li>
-            <li className="uppercase text-xs ">
-              <NavLink to="/gallery" className="tracking-widest px-4 py-1">
-                Gallery
-              </NavLink>
-            </li>
-            <li className="uppercase text-xs ">
-              <NavLink to="/projects" className="tracking-widest px-4 py-1">
-                Projects
-              </NavLink>
-            </li>
-            <li className="uppercase text-xs ">
-              <NavLink
-                to="/certifications"
-                className="tracking-widest px-4 py-1"
-              >
-                Certifications
-              </NavLink>
-            </li>
-            <li className="uppercase text-xs ">
-              <NavLink to="/contacts" className="tracking-widest px-4 py-1">
-                Contacts
-              </NavLink>
-            </li>
+        <button type="button" className="sm:hidden text-3xl">
+          <i className="fa-solid fa-bars" />
+        </button>
+        <nav className="w-full sm:w-max">
+          <ul className="p-0 text-[var(--CL-primary-gray)] sm:hidden grid w-full mt-2">
+            {NavigationLinks.navLinks.map((navigation) => (
+              <li className="uppercase text-xs w-full" key={navigation.id}>
+                <NavLink
+                  to={navigation.to}
+                  className="tracking-widest px-3 py-3 block font-bold"
+                >
+                  {navigation.label}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <ul className="p-0 text-[var(--CL-primary-gray)] items-center hidden sm:flex md:gap-5">
+            {NavigationLinks.navLinks.map((navigation) => (
+              <li className="uppercase text-xs" key={navigation.id}>
+                <NavLink
+                  to={navigation.to}
+                  className="tracking-widest px-4 py-1"
+                >
+                  {navigation.label}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
