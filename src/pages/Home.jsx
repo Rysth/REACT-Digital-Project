0f0000dce4882eafddacd9cc1cd57a047cf4df0e
Rysth <johnpalacios.t@gmail.com>
2023-10-19 13:19:26 -0500

    Complete right section.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Portrait from '../assets/images/portrait/portrait.png';
 
 function Home() {
   return (
     <section>
-      <div className="container max-w-screen-xl p-4 mx-auto">
+      <div className="container max-w-screen-xl p-4 mx-auto text-[var(--CL-primary-gray)]">
         <div className="grid md:grid-cols-2">
           <header className="mb-5 text-center md:text-left md:flex md:flex-col md:justify-center">
             <h2 className="text-2xl md:text-3xl xl:text-4xl font-light text-[var(--CL-primary-light)] uppercase lg:mt-48">
               Project
             </h2>
-            <h2 className="text-4xl font-bold lg:text-5xl xl:text-6xl">
+            <h2 className="text-4xl font-bold lg:text-5xl xl:text-6xl ">
               Primal Statement
             </h2>
             <div className="relative flex items-center justify-center w-full gap-3 mx-auto my-3 md:justify-start md:mt-10 lg:mt-20">
@@ -28,7 +29,7 @@ function Home() {
               </button>
               <hr className="absolute top-0 right-0 w-full md:w-4/5 translate-y-[18.5px] z-10 lg:translate-y-[1.70rem]" />
             </div>
-            <div className="flex items-center justify-end md:justify-start md:mt-4 lg:mt-20 lg:gap-8">
+            <div className="flex items-center justify-end md:justify-start md:mt-4 lg:mt-20 lg:gap-4">
               <p className="text-base text-[var(--CL-primary-light)] sm:text-lg lg:text-2xl">
                 01
               </p>
@@ -40,6 +41,13 @@ function Home() {
           </header>
           <div className="relative">
             <img src={Portrait} alt="" />
+            <Link
+              to="/"
+              className="absolute bottom-0 left-0 px-8 py-5 text-xs tracking-widest uppercase bg-white text-[var(--CL-primary-gray)]"
+            >
+              View Project
+              <i className="ml-3 fa-solid fa-arrow-right" />
+            </Link>
           </div>
         </div>
       </div>
