6df0b06fa846468c548845a4cc1583eb7885b63a
Rysth <johnpalacios.t@gmail.com>
2023-10-26 17:11:45 -0500

    Install react-hook-form library and add basic validations.

--- src/pages/sections/ContactSection.jsx
+++ src/pages/sections/ContactSection.jsx
@@ -1,56 +1,84 @@
+import { useForm } from 'react-hook-form';
 import Subtitle from '../../components/Subtitle/Subtitle';
 import Banner from '../../assets/images/projects/project_5.jpg';
 
 function ContactSection() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data); // Form data is available here
+  };
+
   return (
     <div>
       <div className="container max-w-screen-xl p-4 mx-auto text-[var(--CL-primary-gray)] animate__animated animate__fadeIn animate__slow my-10 md:my-20">
         <Subtitle title="Contact Us" />
         <div className="grid lg:grid-cols-[40%_1fr] my-10 gap-7">
-          <form className="grid gap-3" method="post">
+          {/* eslint-disable */}
+          <form className="grid gap-3" onSubmit={handleSubmit(onSubmit)}>
             <fieldset className="border-0 outline-0">
               <label htmlFor="name">
                 <input
-                  required
                   type="text"
-                  name="name"
-                  id="name"
                   placeholder="Name"
                   className="w-full px-6 py-4 bg-[var(--CL-neutral-light)]"
+                  {...register('name', {
+                    required: true,
+                    pattern: /^[A-Za-z ]+$/i,
+                  })}
+                  aria-invalid={errors.name ? 'true' : 'false'}
                 />
+                {errors.name?.type === 'required' && (
+                  <p className="mt-1 mb-0 text-xs italic text-red-400">
+                    Name is required
+                  </p>
+                )}
               </label>
             </fieldset>
             <fieldset className="border-0 outline-0">
               <label htmlFor="phone_number">
                 <input
-                  required
+                  {...register('phone_number', {
+                    required: true,
+                    pattern: /^[0-9]+$/i,
+                    minLength: 5,
+                    maxLength: 18,
+                  })}
                   type="tel"
-                  name="phone_number"
-                  id="phone_number"
                   placeholder="Phone Number"
                   className="w-full px-6 py-4 bg-[var(--CL-neutral-light)]"
                 />
+                {errors.phone_number?.type === 'required' && (
+                  <p className="mt-1 mb-0 text-xs italic text-red-400">
+                    Phone Number is required and only accept numbers "0-9".
+                  </p>
+                )}
               </label>
             </fieldset>
             <fieldset className="border-0 outline-0">
               <label htmlFor="email">
                 <input
-                  required
+                  {...register('email', { required: true })}
                   type="email"
-                  name="email"
-                  id="email"
                   placeholder="Email"
                   className="w-full px-6 py-4 bg-[var(--CL-neutral-light)]"
                 />
+                {errors.email?.type === 'required' && (
+                  <p className="mt-1 mb-0 text-xs italic text-red-400">
+                    Email is required "name@example.com"
+                  </p>
+                )}
               </label>
             </fieldset>
             <fieldset className="border-0 outline-0">
               <label htmlFor="interest">
                 <input
-                  required
+                  {...register('interest')}
                   type="text"
-                  name="interest"
-                  id="interest"
                   placeholder="Interested In"
                   className="w-full px-6 py-4 bg-[var(--CL-neutral-light)]"
                 />
@@ -59,10 +87,7 @@ function ContactSection() {
             <fieldset className="border-0 outline-0">
               <label htmlFor="message">
                 <textarea
-                  name="message"
-                  id="message"
-                  cols="30"
-                  rows="10"
+                  {...register('message')}
                   placeholder="Message"
                   className="w-full px-6 py-4 bg-[var(--CL-neutral-light)] min-h-[180px] max-h-[180px] min-w-full resize-none"
                 />
@@ -78,6 +103,7 @@ function ContactSection() {
               </button>
             </fieldset>
           </form>
+          {/* eslint-enable */}
           <picture className="col-start-1 col-end-2 row-start-1 row-end-2 max-h-60 lg:max-h-full lg:row-start-1 lg:col-start-2">
             <img src={Banner} alt="" className="object-cover w-full h-full" />
           </picture>
